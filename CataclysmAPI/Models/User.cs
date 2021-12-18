7744433d6bc26b759f6ccbf08c439b40f063e195
Mitch <untoldtitan3k@gmail.com>
2021-12-18 11:07:53 -0700

    Updated thingys. VS is a pain in the ass sometimes.

---
+++ CataclysmAPI/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace CataclysmAPI.Models
+{
+    public partial class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("token")]
+        public int Token { get; set; }
+    }
+}
---
+++ CataclysmAPI/Startup.cs
@@ -0,0 +1,55 @@
+// Unused usings removed
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+//using CataclysmAPI.Models;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace CataclysmAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+
+            //services.AddDbContext<CoreDbContext>(opt =>
+            //                                   opt.UseSqlServer(Configuration.GetConnectionString("Database")));
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
