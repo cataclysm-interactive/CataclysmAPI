75c972584ad893544bfe2a366d0e4facf7624d8e
cataclysm-interactive <95239376+cataclysm-interactive@users.noreply.github.com>
2022-03-12 21:56:57 -0700

    I hope this works....

--- CataclysmAPI/Startup.cs
+++ CataclysmAPI/Startup.cs
@@ -31,8 +31,21 @@ namespace CataclysmAPI
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseSwagger();
+            var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                builder =>
+                                {
+                                    builder.WithOrigins("http://example.com",
+                                              "http://www.contoso.com");
+                                });
+            });
+            app.UseSwagger();
+            app.UseCors(MyAllowSpecificOrigins);
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
